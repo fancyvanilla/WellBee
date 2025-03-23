a11a2d01347c40f59db8557f34ff5b88684551d1
Mariem BenYounes <maryemby21@gmail.com>
2025-03-23 15:41:29 +0100

    set up mysql

---
+++ src/main/java/tn/fst/team2/jee/wellbee/WellbeeApplication.java
@@ -0,0 +1,13 @@
+package tn.fst.team2.jee.wellbee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WellbeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WellbeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tn/fst/team2/jee/wellbee/WellbeeApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.fst.team2.jee.wellbee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WellbeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
