5c159c182564d11204e7ce671825f234da6a39dd
Mariem BenYounes <maryemby21@gmail.com>
2025-03-29 01:43:57 +0100

    fix: enabled jpa auditing

--- src/main/java/tn/fst/team2/jee/wellbee/WellbeeApplication.java
+++ src/main/java/tn/fst/team2/jee/wellbee/WellbeeApplication.java
@@ -1,16 +1,16 @@
 package tn.fst.team2.jee.wellbee;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.stereotype.Repository;
+import tn.fst.team2.jee.wellbee.mappers.UserMapper;
 
-@SpringBootApplication
-@ComponentScan(basePackages = "tn.fst.team2.jee")
-@EnableJpaRepositories(basePackages = "tn.fst.team2.jee")
-@EntityScan(basePackages = "tn.fst.team2.jee")
+@SpringBootApplication(scanBasePackages={"tn.fst.team2.jee.wellbee"})
+@EnableJpaAuditing
 public class WellbeeApplication {
 
 	public static void main(String[] args) {
